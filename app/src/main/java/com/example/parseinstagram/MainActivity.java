441463c1ec9c012ed7fa04d93b0e6c94bcb6dc21
whis99 <hanswhisler@gmail.com>
2022-09-03 16:21:05 -0400

    post class, querying post

--- app/src/main/java/com/example/parseinstagram/MainActivity.java
+++ app/src/main/java/com/example/parseinstagram/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
+    private ImageView postImg;
+    private EditText description;
+    private Button pictureBtn;
+    private Button submitBtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +36,32 @@ public class MainActivity extends AppCompatActivity {
                 Log.e(TAG, e.getLocalizedMessage());
             }else{
                 Log.d(TAG,"Object saved.");
+            }
+        });
+
+        postImg = findViewById(R.id.imageInput);
+        description = findViewById(R.id.descriptionImg);
+        pictureBtn = findViewById(R.id.picBtn);
+        submitBtn = findViewById(R.id.submitBtn);
+        
+        queryPost();
+
+    }
+
+    private void queryPost() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for(Post post: posts){
+                    Log.i(TAG, "Post" + post.getDescription() + ", username: " + post.getUser().getUsername());
+
+                }
             }
         });
     }
--- app/src/main/java/com/example/parseinstagram/ParseApplication.java
+++ app/src/main/java/com/example/parseinstagram/ParseApplication.java
@@ -3,12 +3,17 @@ package com.example.parseinstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     // Initializes Parse SDK as soon the application is created
     @Override
     public void onCreate() {
         super.onCreate();
+
+        //Register your parse model
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
                 .clientKey(getString(R.string.back4app_client_key))
